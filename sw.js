fbbec7c7f57c9ef972afffc280121b6741acc35a
Nizo <mrojers534@gmail.com>
2026-06-07 19:25:43 +0400

    Remove service worker completely to fix stale cache

--- sw.js
+++ sw.js
@@ -1,28 +1,10 @@
-const CACHE = 'leyla-dict-v4';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+// Unregister all caches and self
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  // только GET запросы
-  if (e.request.method !== 'GET') return;
-
-  e.respondWith(
-    fetch(e.request).then(response => {
-      if (response && response.status === 200) {
-        const clone = response.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, clone));
-      }
-      return response;
-    }).catch(() => caches.match(e.request))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
   );
 });
